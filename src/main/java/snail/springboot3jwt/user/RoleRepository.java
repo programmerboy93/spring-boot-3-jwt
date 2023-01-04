b202452152bbe919baf115bd931b7a95b5a277fb
lukasz <lukaszsikorski19933@gmail.com>
2023-01-04 16:24:17 +0100

    Add repository for user's role

---
+++ src/main/java/snail/springboot3jwt/user/RoleRepository.java
@@ -0,0 +1,10 @@
+package snail.springboot3jwt.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(ERole name);
+}
