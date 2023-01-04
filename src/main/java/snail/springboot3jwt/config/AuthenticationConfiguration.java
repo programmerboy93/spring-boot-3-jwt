a6502c2cb8e5c39f9f964c2d3d46ca08c08e79fe
lukasz <lukaszsikorski19933@gmail.com>
2023-01-04 16:23:36 +0100

    Add configuration for authentication

---
+++ src/main/java/snail/springboot3jwt/config/AuthenticationConfiguration.java
@@ -0,0 +1,23 @@
+package snail.springboot3jwt.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class AuthenticationConfiguration {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}
