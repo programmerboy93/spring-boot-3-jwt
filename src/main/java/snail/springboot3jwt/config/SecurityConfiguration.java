55e511515048f40ce7a437710e2816b7fc9261a0
lukasz <lukaszsikorski19933@gmail.com>
2023-01-04 18:19:05 +0100

    Add jwt filter to config

--- src/main/java/snail/springboot3jwt/config/SecurityConfiguration.java
+++ src/main/java/snail/springboot3jwt/config/SecurityConfiguration.java
@@ -1,14 +1,20 @@
 package snail.springboot3jwt.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import snail.springboot3jwt.auth.JwtAuthenticationFilter;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -21,7 +27,11 @@ public class SecurityConfiguration {
                 .anyRequest()
                 .authenticated();
 
-        http.sessionManagement().sessionCreationPolicy(STATELESS);
+        http
+                .sessionManagement()
+                .sessionCreationPolicy(STATELESS)
+                .and()
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
