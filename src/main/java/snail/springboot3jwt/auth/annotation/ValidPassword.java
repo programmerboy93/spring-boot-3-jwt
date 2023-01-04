e0548edff47ab774425b7672d55e0f4ce74a9829
lukasz <lukaszsikorski19933@gmail.com>
2023-01-04 17:51:43 +0100

    Add ValidPassword annotation

---
+++ src/main/java/snail/springboot3jwt/auth/annotation/ValidPassword.java
@@ -0,0 +1,18 @@
+package snail.springboot3jwt.auth.annotation;
+
+import jakarta.validation.Constraint;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Documented
+@Retention(RUNTIME)
+@Target(FIELD)
+@Constraint(validatedBy = PasswordConstraintValidator.class)
+public @interface ValidPassword {
+    String message() default "Invalid Password";
+}
