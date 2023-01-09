ae46f3376dc79640269b5f865fbf68d358802001
lukasz <lukaszsikorski19933@gmail.com>
2023-01-09 14:49:12 +0100

    Change ValidPassword

--- src/main/java/snail/springboot3jwt/auth/annotation/ValidPassword.java
+++ src/main/java/snail/springboot3jwt/auth/annotation/ValidPassword.java
@@ -1,18 +1,21 @@
 package snail.springboot3jwt.auth.annotation;
 
 import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Documented
 @Retention(RUNTIME)
-@Target(FIELD)
+@Target({ TYPE, FIELD, ANNOTATION_TYPE })
 @Constraint(validatedBy = PasswordConstraintValidator.class)
 public @interface ValidPassword {
     String message() default "Invalid Password";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
 }
