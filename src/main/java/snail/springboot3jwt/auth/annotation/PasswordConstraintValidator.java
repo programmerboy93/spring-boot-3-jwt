886d000cde9504ea64162d8f9dd0c6bb070375a0
lukasz <lukaszsikorski19933@gmail.com>
2023-01-09 18:02:47 +0100

    Test PasswordConstraintValidator

--- src/main/java/snail/springboot3jwt/auth/annotation/PasswordConstraintValidator.java
+++ src/main/java/snail/springboot3jwt/auth/annotation/PasswordConstraintValidator.java
@@ -2,13 +2,10 @@ package snail.springboot3jwt.auth.annotation;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
-import lombok.SneakyThrows;
 import org.passay.*;
 
-import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Properties;
 
 
 public class PasswordConstraintValidator implements ConstraintValidator<ValidPassword, String> {
@@ -18,38 +15,17 @@ public class PasswordConstraintValidator implements ConstraintValidator<ValidPas
 
     }
 
-    @SneakyThrows
     @Override
-    public boolean isValid(String password, ConstraintValidatorContext context) {
+    public boolean isValid(String password, final ConstraintValidatorContext context) {
 
-        Properties props = new Properties();
-        InputStream inputStream = getClass()
-                .getClassLoader().getResourceAsStream("passay.properties");
-        props.load(inputStream);
-        MessageResolver resolver = new PropertiesMessageResolver(props);
-
-        PasswordValidator validator = new PasswordValidator(resolver, Arrays.asList(
+        final PasswordValidator validator = new PasswordValidator(Arrays.asList(
                 new LengthRule(8, 30),
-
-                // at least one upper-case character
                 new CharacterRule(EnglishCharacterData.UpperCase, 1),
-
-                // at least one lower-case character
                 new CharacterRule(EnglishCharacterData.LowerCase, 1),
-
-                // at least one digit character
                 new CharacterRule(EnglishCharacterData.Digit, 1),
-
-                // at least one symbol (special character)
                 new CharacterRule(EnglishCharacterData.Special, 1),
-
-                // no whitespace
                 new WhitespaceRule(),
-
-                // rejects passwords that contain a sequence of >= 5 characters alphabetical  (e.g. abcdef)
                 new IllegalSequenceRule(EnglishSequenceData.Alphabetical, 5, false),
-
-                // rejects passwords that contain a sequence of >= 5 characters numerical   (e.g. 12345)
                 new IllegalSequenceRule(EnglishSequenceData.Numerical, 5, false)
         ));
 
