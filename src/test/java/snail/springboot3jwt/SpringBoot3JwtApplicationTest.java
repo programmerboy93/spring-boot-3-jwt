5b0a469680a0ea89f85c30751655885ca1b4e16f
lukasz <lukaszsikorski19933@gmail.com>
2023-01-12 20:20:50 +0100

    Test SpringBootApplication with

---
+++ src/test/java/snail/springboot3jwt/SpringBoot3JwtApplicationTest.java
@@ -0,0 +1,15 @@
+package snail.springboot3jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class SpringBoot3JwtApplicationTest {
+
+    @Test
+    void main() {
+
+    }
+}
