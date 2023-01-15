122c21c65b4f1ffad3c3acaea35a65b2e4afc988
lukasz <lukaszsikorski19933@gmail.com>
2023-01-16 00:25:52 +0100

    Add relation ManyToMany

--- src/main/java/snail/springboot3jwt/film/Country.java
+++ src/main/java/snail/springboot3jwt/film/Country.java
@@ -1,11 +1,15 @@
 package snail.springboot3jwt.film;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -16,5 +20,8 @@ import lombok.NoArgsConstructor;
 public class Country extends BaseEntity {
 
     private String country;
+
+    @ManyToMany
+    private Set<Film> films = new HashSet<>();
 
 }
